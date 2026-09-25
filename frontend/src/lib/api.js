f29a6a9318f5da33d6996727ac3ff7d9da257bcc
otepaleon45-cloud <otepaleon45@gmail.com>
2026-09-25 23:35:32 +0300

    Update API URL to use VITE_API_URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
+export const API = `${import.meta.env.VITE_API_URL || 'https://school-admin-hub-3.onrender.com'}/api`;
+
 
 const api = axios.create({ baseURL: API });
 
